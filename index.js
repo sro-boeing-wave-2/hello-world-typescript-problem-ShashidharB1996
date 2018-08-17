4242b454c347748918217baffe58e561e5736939
Shashhidhar B <shashishashidhar70@gmail.com>
2018-08-17 10:28:19 +0530

    Done the assignment

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("sayHelloBtn").addEventListener("click", Clickevent);
+function Clickevent() {
+    document.getElementById("helloWorldBlock").innerHTML = "<h1><b> Hello World </b></h1>";
+}
